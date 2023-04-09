8517cbff3158515d0a9b77e17c195613b4fcfdc4
Ariel Cabeza <cervanteslibch@gmail.com>
2023-04-09 17:57:38 -0300

    Cree icono de boton de nuevo gasto

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-
-const ControlPresupuesto = ({presupuesto}) => {
-
-  const formatearCantidadAPeso = (cantidad) => {
-    return cantidad.toLocaleString('es-AR', {
-      style: 'currency',
-      currency: 'ARS'
-    })
-  }
 
 
-  return (
-    <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
-        <div>Aqui el Grafico</div>
-        <div className='contenido-presupuesto'>
-            <p>
-                <span>Presupuesto:</span> {formatearCantidadAPeso(presupuesto)} 
-            </p>
-            <p>
-                <span>Disponible:</span> {formatearCantidadAPeso(0)} 
-            </p>
-            <p>
-                <span>Gastado:</span> {formatearCantidadAPeso(0)} 
-            </p>
-        </div>
-    </div>
-  )
+const ControlPresupuesto = ({ presupuesto }) => {
+
+	const formatearCantidadAPeso = (cantidad) => {
+		return cantidad.toLocaleString('es-AR', {
+			style: 'currency',
+			currency: 'ARS'
+		})
+	}
+
+
+	return (
+		<div className='contenedor-presupuesto contenedor sombra dos-columnas'>
+			<div>Aqui el Grafico</div>
+			<div className='contenido-presupuesto'>
+				<p>
+					<span>Presupuesto:</span> {formatearCantidadAPeso(presupuesto)}
+				</p>
+				<p>
+					<span>Disponible:</span> {formatearCantidadAPeso(0)}
+				</p>
+				<p>
+					<span>Gastado:</span> {formatearCantidadAPeso(0)}
+				</p>
+			</div>
+		</div>
+	)
 }
 
 export default ControlPresupuesto
