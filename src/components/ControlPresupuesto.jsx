9be18aa9732b319db50db318c5bac5af58f9cf52
Ariel Cabeza <cervanteslibch@gmail.com>
2023-04-09 17:44:52 -0300

    Formatee la cantidad a peso argentino

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 
 const ControlPresupuesto = ({presupuesto}) => {
+
+  const formatearCantidadAPeso = (cantidad) => {
+    return cantidad.toLocaleString('es-AR', {
+      style: 'currency',
+      currency: 'ARS'
+    })
+  }
+
+
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
         <div>Aqui el Grafico</div>
         <div className='contenido-presupuesto'>
             <p>
-                <span>Presupuesto:</span> ${presupuesto} 
+                <span>Presupuesto:</span> {formatearCantidadAPeso(presupuesto)} 
+            </p>
+            <p>
+                <span>Disponible:</span> {formatearCantidadAPeso(0)} 
+            </p>
+            <p>
+                <span>Gastado:</span> {formatearCantidadAPeso(0)} 
             </p>
         </div>
     </div>
